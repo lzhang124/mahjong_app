d4661bff52ccc0355172ab5dff3bd519f76ec83b
Larry Zhang <lzhang124@gmail.com>
2023-01-14 13:38:17 -0500

    test hello world

--- api/base_app.py
+++ api/base_app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/')
+def hello_world():
+    return '<p>Hello, World!</p>'
+
+
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'health': True})
