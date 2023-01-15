be9985b3b5920fba1b8ad517d4ae8679f86ede68
Larry Zhang <lzhang124@gmail.com>
2023-01-15 16:10:57 -0500

    remove log

--- api/db/db.py
+++ api/db/db.py
@@ -1,13 +1,7 @@
-import logging
-
 import psycopg2
 
 from api.config.config import DB_URI
 
 
-logger = logging.getLogger(__name__)
-
-
 def db_conn():
-    logger.info(f'DB_URI={DB_URI}')
     return psycopg2.connect(DB_URI)
